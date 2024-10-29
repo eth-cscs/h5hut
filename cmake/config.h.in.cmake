4a3412019a773f783f2b8b4c26e9c777614bce91
John Biddiscombe <biddisco@cscs.ch>
2024-10-29 15:41:59 +0100

    Add initial cmake support, fortran not yet supported

---
+++ cmake/config.h.in.cmake
@@ -0,0 +1,24 @@
+/* Name of package */
+#define PACKAGE "@PROJECT_NAME@"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "h5part@lists.psi.ch"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "@PROJECT_NAME@"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "@PROJECT_NAME@ @PROJECT_VERSION@"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "@PROJECT_NAME@"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "@PROJECT_VERSION@"
+
+/* Version number of package */
+#define VERSION "@PROJECT_VERSION@"
+
